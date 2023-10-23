7baac0e04112b04ef2a1b31a5af0f5b8177ba6f2
nesty11 <arielvale8@hotmail.com>
2023-10-23 18:30:50 -0400

    movie card prop onClick feature

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,11 @@
-export const BookCard = (props) =>{
-    return <div>{props.bookData.title}</div>;
-}
+export const MovieCard = ({movie, onMovieClick}) => {
+    return (
+        <div
+        onClick={() => {
+            onMovieClick(movie);
+        }}
+        >
+            {movie.title}
+        </div>
+    );
+};
