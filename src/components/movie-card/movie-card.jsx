e5aa9257e28c45abba4199d3ae40a72bf004bfe5
nesty11 <arielvale8@hotmail.com>
2023-10-23 19:20:06 -0400

    formatted/cleaned code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,11 @@
-export const MovieCard = ({movie, onMovieClick}) => {
-    return (
-        <div
-        onClick={() => {
-            onMovieClick(movie);
-        }}
-        >
-            {movie.title}
-        </div>
-    );
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 
 //Import statement to indicate that you need to bundle ./index.scss
 import "./index.scss";
 
 const App = () => {
-    return <MainView />;
+  return <MainView />;
 };
 
 //Finds the root of your app
