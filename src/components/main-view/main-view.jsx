cc2cde6cdbc39260010d6987fbc7a4082019baed
nesty11 <arielvale8@hotmail.com>
2023-10-23 19:21:38 -0400

    changed to lowercase 'description' and 'genre'
    properties now have correct lowercase component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,46 +7,51 @@ export const MainView = () => {
     {
       id: 1,
       title: "Star Wars: Revenge of the Sith",
-      Description:
+      description:
         "Three years into the Clone Wars, Obi-Wan pursues a new threat, while Anakin is lured by Chancellor Palpatine into a sinister plot to rule the galaxy.",
-      Genre: "Action",
-      image: "https://www.themoviedb.org/t/p/original/nduFqlQF4cAFJ3OIfrpsISDvoxq.jpg",
+      genre: "Action",
+      image:
+        "https://www.themoviedb.org/t/p/original/nduFqlQF4cAFJ3OIfrpsISDvoxq.jpg",
       director: "George Lucas",
     },
     {
       id: 2,
       title: "Dungeons & Dragons: Honor Among Thieves",
-      Description:
+      description:
         "A charming thief and a band of unlikely adventurers embark on an epic quest to retrieve a lost relic, but things go dangerously awry when they run afoul of the wrong people.",
-      Genre: "Fantasy",
-      image: "https://www.themoviedb.org/t/p/original/tzUrEepcNv20YrS2jlBY4B6sMZX.jpg",
+      genre: "Fantasy",
+      image:
+        "https://www.themoviedb.org/t/p/original/tzUrEepcNv20YrS2jlBY4B6sMZX.jpg",
       director: "John Francis Daley",
     },
     {
       id: 3,
       title: "Princess Mononoke",
-      Description:
+      description:
         "On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.",
-      Genre: "Adventure",
-      image: "https://www.themoviedb.org/t/p/original/58LcltHMRBICiavCzpAcI2YQvhQ.jpg",
+      genre: "Adventure",
+      image:
+        "https://www.themoviedb.org/t/p/original/58LcltHMRBICiavCzpAcI2YQvhQ.jpg",
       director: "Hayao Miyazaki",
     },
     {
       id: 4,
       title: "Spider-Man: No Way Home",
-      Description:
+      description:
         "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
-      Genre: "Action",
-      image: "https://www.themoviedb.org/t/p/original/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
+      genre: "Action",
+      image:
+        "https://www.themoviedb.org/t/p/original/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
       director: "Jon Watts",
     },
     {
       id: 5,
       title: "Spider-Man: Across The Spider-Verse",
-      Description:
+      description:
         "Miles Morales catapults across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence. When the heroes clash on how to handle a new threat, Miles must redefine what it means to be a hero.",
-      Genre: "Animation",
-      image: "https://www.themoviedb.org/t/p/original/zknrogDlwcmaz3yHkA3yEhy005t.jpg",
+      genre: "Animation",
+      image:
+        "https://www.themoviedb.org/t/p/original/zknrogDlwcmaz3yHkA3yEhy005t.jpg",
       director: "Joaquim Dos Santos",
     },
   ]);
@@ -67,13 +72,15 @@ export const MainView = () => {
   return (
     <div>
       {movies.map((movie) => {
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />;
+        return (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        );
       })}
     </div>
   );
