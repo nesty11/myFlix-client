b8b9f3a6f993b19fe8f5f071cd60d1750d1f6c7e
nesty11 <arielvale8@hotmail.com>
2023-11-01 16:25:55 -0400

    merged branches
    fixes director property

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27191,8 +27191,8 @@ const MainView = ()=>{
                     },
                     Director: {
                         Name: movie.Director.Name,
-                        Bio: Director.Bio,
-                        Birth: Director.Birth
+                        Bio: movie.Director.Bio,
+                        Birth: movie.Director.Birth
                     },
                     Featured: movie.Featured
                 };
@@ -28291,7 +28291,7 @@ const MovieView = ({ movie, onBackClick })=>{
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("span", {
-                        children: movie.Genre
+                        children: movie.Genre.Name
                     }, void 0, false, {
                         fileName: "src/components/movie-view/movie-view.jsx",
                         lineNumber: 17,
@@ -28313,7 +28313,7 @@ const MovieView = ({ movie, onBackClick })=>{
                         columnNumber: 9
                     }, undefined),
                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("span", {
-                        children: movie.Director
+                        children: movie.Director.Name
                     }, void 0, false, {
                         fileName: "src/components/movie-view/movie-view.jsx",
                         lineNumber: 21,
