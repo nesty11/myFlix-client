cde1830fa4bdb838396f79bfea7cb83c446d207f
nesty11 <arielvale8@hotmail.com>
2023-11-02 18:30:45 -0400

    added local storage for user and token
    also formatted code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,62 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch("https://nvmyflix-06d7b8d88193.herokuapp.com/movies")
-    .then((response) => response.json())
-    .then((data) => {
-      const moviesFromApi = data.map((movie) => {
-        return {
-          _id: movie.id,
+    if (!token) {
+      return;
+    }
+    fetch("https://nvmyflix-06d7b8d88193.herokuapp.com/movies", {
+      hearders: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie.id,
             Title: movie.Title,
             Description: movie.Description,
             Genre: {
               Name: movie.Genre.Name,
-              Description: movie.Genre.Description
+              Description: movie.Genre.Description,
             },
             Director: {
               Name: movie.Director.Name,
               Bio: movie.Director.Bio,
-              Birth: movie.Director.Birth
+              Birth: movie.Director.Birth,
             },
-            Featured: movie.Featured
-        };
+            Featured: movie.Featured,
+          };
+        });
+        setMovies(moviesFromApi);
       });
-      setMovies(moviesFromApi);
-    });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -42,10 +68,30 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+        <div>The list is empty!</div>;
+      </>
+    );
   }
   return (
     <div>
+      <button
+        onClick={() => {
+          setUser(null);
+        }}
+      >
+        Logout
+      </button>
       {movies.map((movie) => {
         return (
           <MovieCard
