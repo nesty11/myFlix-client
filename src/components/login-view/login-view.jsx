f7880987e236f01700e8708cca44dbed621ddef9
nesty11 <arielvale8@hotmail.com>
2023-11-03 18:40:42 -0400

    data updated, fixed url, removed title, updated password pattern
    data properties updated to correct ones, url endpoint fixed, removed authorization and added content-type, formatted coded, removed title property, and fixed password pattern property

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,21 +9,22 @@ export const LoginView = ({ onLoggedIn }) => {
     event.preventDefault();
 
     const data = {
-      access: username,
-      secret: password,
+      Username: username,
+      Password: password,
     };
 
-    fetch("https://nvmyflix-06d7b8d88193.herokuapp.com", {
+    fetch("https://nvmyflix-06d7b8d88193.herokuapp.com/login", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        //Content type to be replaced in the future
       },
       body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
+    }).then((response) => response.json())
       .then((data) => {
         console.log("Login response: ", data);
         if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
           localStorage.setItem("token", data.token);
           onLoggedIn(data.user, data.token);
         } else {
@@ -55,8 +56,7 @@ export const LoginView = ({ onLoggedIn }) => {
           onChange={(e) => setPassword(e.target.value)}
           required
           minLength="6"
-          pattern="^(?=.*[0-9])(?=.*[!@#$%^&*])"
-          title="Password must contain at least one number and one symbol."
+          pattern="^(?=.*[0-9])(?=.*[!@#$%^&*]).{6,}$"
         />
       </label>
       <button type="submit">Submit</button>
