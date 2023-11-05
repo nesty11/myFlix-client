4db61630e644e787b5a9aecb001a1976c8967e8f
nesty11 <arielvale8@hotmail.com>
2023-11-05 18:45:50 -0500

    added containers by react bootstrap
    imported index.scss file and react bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
+import "./index.scss";
 
 //Import statement to indicate that you need to bundle ./index.scss
 import "./index.scss";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 //Finds the root of your app
