0692887917c91d7a5dc7074a9df0238a7e0b78c8
nesty11 <arielvale8@hotmail.com>
2023-11-06 17:13:34 -0500

    deleted duplicate code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import Container from "react-bootstrap/Container";
-import "./index.scss";
 
 //Import statement to indicate that you need to bundle ./index.scss
 import "./index.scss";
