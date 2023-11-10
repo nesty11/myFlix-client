e9f34dd67a072fb9159c3f75b4f8bc2cab760943
nesty11 <arielvale8@hotmail.com>
2023-11-09 19:15:25 -0500

    formatted director birthdate
    now director birthdate displays only month, day and year (i.e. Oct 6, 1970)

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,12 +3,21 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies, user }) => {
-  const { movieTitle } = useParams();
-  const movie = movies.find((m) => m.Title === movieTitle);
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+  const movie = movies.find((m) => m._id === movieId);
+  /* const movie = movies.find((m) => m.Title === movieTitle); */
   if (!movie) {
     return <div>Movie not found.</div>;
   }
+
+  const directorBirthDate = new Date(movie.Director.Birth);
+  const formattedDirectorBirthDate = `${directorBirthDate.toLocaleString(
+    "en-us",
+    {
+      month: "short",
+    }
+  )} ${directorBirthDate.getDate()}, ${directorBirthDate.getFullYear()}`;
 
   return (
     <div>
@@ -27,11 +36,11 @@ export const MovieView = ({ movies, user }) => {
             <strong>Director's Bio:</strong> {movie.Director.Bio}
           </li>
           <li>
-            <strong>Director's Birth:</strong> {movie.Director.Birth}
+            <strong>Director's Birth:</strong> {formattedDirectorBirthDate}
           </li>
         </ul>
       </div>
-      {movie.Featured && <div>Featured Movie</div>}
+      {movie.Featured && <div>This is a Featured Movie!</div>}
       <Link to={`/`}>
         <button className="back-button">Back</button>
       </Link>
