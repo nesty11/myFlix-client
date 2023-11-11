5fb19f0d21dd7bbe4f88124baf9278a9e0dda7d3
nesty11 <arielvale8@hotmail.com>
2023-11-10 19:45:41 -0500

    fixed _id property

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -29,7 +29,7 @@ export const MainView = () => {
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
           return {
-            _id: movie.id,
+            _id: movie._id,
             Title: movie.Title,
             Description: movie.Description,
             Genre: {
